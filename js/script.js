b94656ef8af89cd6ae3c7cca381a7908407575f3
SergeyMavrin <mavrin_sergei@list.ru>
2022-09-21 11:17:29 +0400

    frontend

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const swiper = new Swiper('.swiper', {
         // when window width is >= 320px
         320: {
             slidesPerView: 2,
-            spaceBetween: 30
+            spaceBetween: 10
         },
         // when window width is >= 480px
         992: {
