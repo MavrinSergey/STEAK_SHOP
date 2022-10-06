5c91184046c755a977ec1d71e6337a501fcaef56
SergeyMavrin <mavrin_sergei@list.ru>
2022-10-06 21:09:37 +0400

    test05_form

--- telegram.php
+++ telegram.php
@@ -1,26 +1,26 @@
 <?php
 $name = $_POST['user_name']; // input name
-$phone = $_POST['user_email']; // input phone
-$email = $_POST['user_phone']; // input phone
+$email = $_POST['user_email']; // input phone
+$phone = $_POST['user_phone']; // input phone
 // Токен телеграм бота
-$token = '5663607956:AAFrkIlG6zmlTZ8VVeiLrqQ8lD7Hn5c9uQQ';
+$token = "5663607956:AAFrkIlG6zmlTZ8VVeiLrqQ8lD7Hn5c9uQQ";
 // ID чата
-$chat_id = '-805251282';
+$chat_id = "-805251282";
 $arr = array(
-  'Имя пользователя: ' => $name,
+  'Имя: ' => $name,
   'Телефон: ' => $phone,
   'Email: ' => $email,
 );
 
-foreach ($_POST as $key => $val) {
-  $txt .= '<b>'.$key.'</b> '.$value.'%0A';
+foreach ($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTelegram = fopen('http://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
-&parse_mode=htnl&text={$txt}','r');
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
+&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  header('Location: thank-you.html');
+  header('Location: index.html');
 } else {
   echo 'Error';
 }
